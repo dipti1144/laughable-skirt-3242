d68303b0c10e6f597d4bb32e968bf325bbf29881
deepti1144 <dipz09085@gmail.com>
2022-11-09 23:05:14 +0530

    header

---
+++ exchange-mart/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import "./Props.css"
+import Home from "./Components/Home"
+import Header from "./Components/Header"
+
+function App() {
+  return (
+    <div>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
