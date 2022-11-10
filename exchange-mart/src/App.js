ba5600e9618d455ff30fe73f72653cf4a54a4731
deepti1144 <dipz09085@gmail.com>
2022-11-10 19:16:40 +0530

    data

--- exchange-mart/src/App.js
+++ exchange-mart/src/App.js
@@ -4,10 +4,12 @@ import "./Props.css"
 import Home from "./Components/Home"
 import Header from "./Components/Header"
 
+
 function App() {
   return (
     <div>
       <Header/>
+      <Home/>
     </div>
   );
 }
