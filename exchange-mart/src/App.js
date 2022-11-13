c32c69d382aaa44e76a311a0187fc636fa13a858
deepti1144 <dipz09085@gmail.com>
2022-11-13 19:17:07 +0530

    done

--- exchange-mart/src/App.js
+++ exchange-mart/src/App.js
@@ -1,15 +1,24 @@
-
 import './App.css';
-import "./Props.css"
-import Home from "./Components/Home"
-import Header from "./Components/Header"
+import Header from './Components/Header';
+import Product from './Components/Product';
+import {Routes,Route} from "react-router-dom"
+import Signup from './Components/Signup/Signup';
+import Login from './Components/Login/Login';
 
 
 function App() {
   return (
-    <div>
-      <Header/>
-      <Home/>
+
+    <div className="App">
+      <Routes>
+        <Route path="/" element={ <Header/>}></Route>
+        <Route path="/product" element={ <Product/>}></Route>
+        <Route path="/signup" element={ <Signup/>}></Route>
+        <Route path="/login" element={ <Login/>}></Route>
+      </Routes>
+
+    
+     
     </div>
   );
 }
--- exchange-mart/src/index.js
+++ exchange-mart/src/index.js
@@ -4,17 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom"
-import {ChakraProvider} from "@chakra-ui/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <ChakraProvider>
-    <BrowserRouter>
+  <BrowserRouter>
     <App />
-    </BrowserRouter>
-    </ChakraProvider>
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
